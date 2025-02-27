272b25bbb2a5169dff10422674deb920417be629
KJ322 <194835329+KJ322@users.noreply.github.com>
2025-02-27 18:43:57 +0000

    added decision structure for determination

--- skillMod.java
+++ skillMod.java
@@ -38,40 +38,110 @@ public class skillMod
     {
         //switch case written with Claude
         // Using switch with ranges (Java 14+)
+        /* Switch case only works in Java 14 onward
         switch (abilityScore) {
             case 1:
-                return -5;
+                System.out.println("Your ability modifer is -5");
             case 2, 3:
-                return -4;
+                System.out.println("Your ability modifer is -4");
             case 4, 5:
-                return -3;
+                System.out.println("Your ability modifer is -3");
             case 6, 7:
-                return -2;
+                System.out.println("Your ability modifer is -2");
             case 8, 9:
-                return -1;
+                System.out.println("Your ability modifer is -1");
             case 10, 11:
-                return 0;
+                System.out.println("Your ability modifer is 0");
             case 12, 13:
-                return 1;
+                System.out.println("Your ability modifer is 1");
             case 14, 15:
-                return 2;
+                System.out.println("Your ability modifer is 2");
             case 16, 17:
-                return 3;
+                System.out.println("Your ability modifer is 3");
             case 18, 19:
-                return 4;
+                System.out.println("Your ability modifer is 4");
             case 20, 21:
-                return 5;
+                System.out.println("Your ability modifer is 5");
             case 22, 23:
-                return 6;
+                System.out.println("Your ability modifer is 6");
             case 24, 25:
-                return 7;
+                System.out.println("Your ability modifer is 7");
             case 26, 27:
-                return 8;
+                System.out.println("Your ability modifer is 8");
             case 28, 29:
-                return 9;
+                System.out.println("Your ability modifer is 9");
             case 30:
-                return 10;
+                System.out.println("Your ability modifer is 10");
             default:
-                return 0; // Should never reach here given range checks
+                System.out.println("Your ability modifer is 0"); // Should never reach here given range checks
+                */
+        if (abilityScore == 1) 
+        {
+            System.out.println("Your ability modifer is -5");
+        } 
+        else if (abilityScore >= 2 && abilityScore <= 3) 
+        {
+            System.out.println("Your ability modifer is -4");
+        } 
+        else if (abilityScore >= 4 && abilityScore <= 5) 
+        {
+            System.out.println("Your ability modifer is -3");
+        } 
+        else if (abilityScore >= 6 && abilityScore <= 7) 
+        {
+            System.out.println("Your ability modifer is -2");
+        } 
+        else if (abilityScore >= 8 && abilityScore <= 9) 
+        {
+            System.out.println("Your ability modifer is -1");
+        } 
+        else if (abilityScore >= 10 && abilityScore <= 11) 
+        {
+            System.out.println("Your ability modifer is 0");
+        } 
+        else if (abilityScore >= 12 && abilityScore <= 13) 
+        {
+            System.out.println("Your ability modifer is 1");
+        } 
+        else if (abilityScore >= 14 && abilityScore <= 15) 
+        {
+            System.out.println("Your ability modifer is 2");
+        } 
+        else if (abilityScore >= 16 && abilityScore <= 17) 
+        {
+            System.out.println("Your ability modifer is 3");
+        } 
+        else if (abilityScore >= 18 && abilityScore <= 19) 
+        {
+            System.out.println("Your ability modifer is 4");
+        } 
+        else if (abilityScore >= 20 && abilityScore <= 21) 
+        {
+            System.out.println("Your ability modifer is 5");
+        } 
+        else if (abilityScore >= 22 && abilityScore <= 23) 
+        {
+            System.out.println("Your ability modifer is 6");
+        } 
+        else if (abilityScore >= 24 && abilityScore <= 25) 
+        {
+            System.out.println("Your ability modifer is 7");
+        } 
+        else if (abilityScore >= 26 && abilityScore <= 27) 
+        {
+            System.out.println("Your ability modifer is 8");
+        } 
+        else if (abilityScore >= 28 && abilityScore <= 29) 
+        {
+            System.out.println("Your ability modifer is 9");
+        } 
+        else if (abilityScore == 30) 
+        {
+            System.out.println("Your ability modifer is 1");
+        } 
+        else {
+            // Should never reach here with our test range
+            System.out.println("Your ability modifer is 0");
+        }
     }
 }
