3b74dfb5962b534242f7c889b763811b11b9a663
KJ322 <194835329+KJ322@users.noreply.github.com>
2025-03-17 18:38:10 +0000

    before AN put his mitts on it

--- dnd program bits/rollStats.java
+++ dnd program bits/rollStats.java
@@ -1,7 +1,42 @@
 public class rollStats 
 {
+    /*
+     * rolls stats for char sheets
+     * need an intro, roll function, way for players to assign stats
+     */
     public static void main(String[] args) 
     {
-        
+        for (int i = 0; i < 6; i++)
+        {
+            int roll1 = (int)(Math.random() * 6) + 1;
+            int roll2 = (int)(Math.random() * 6) + 1;
+            int roll3 = (int)(Math.random() * 6) + 1;
+            int roll4 = (int)(Math.random() * 6) + 1;
+
+            int rollTotal = (roll1 + roll2 + roll3 + roll4) - ;
+            
+            System.out.println(rollTotal);
+        }
+    }
+
+    public static void intro()
+    {
+        System.out.println("Hello! Let's roll up your character's stats!");
+        System.out.println("This program will roll your stats, and then you'll be able to assign them.");
+    }
+
+    public static void rolls()
+    {
+        for (int i = 0; i < 6; i++)
+        {
+            int roll1 = (int)(Math.random() * 6) + 1;
+            int roll2 = (int)(Math.random() * 6) + 1;
+            int roll3 = (int)(Math.random() * 6) + 1;
+            int roll4 = (int)(Math.random() * 6) + 1;
+
+            int rollTotal = (roll1 + roll2 + roll3 + roll4) - //TODO;
+            
+            System.out.println(rollTotal);
+        }
     }
 }
