1bbbc3d1e6a5ef62ace902edb5b945e4af01554d
KJ322 <194835329+KJ322@users.noreply.github.com>
2025-03-19 17:05:18 +0000

    updated rollStats

--- dnd program bits/rollStats.java
+++ dnd program bits/rollStats.java
@@ -9,39 +9,15 @@ public class rollStats
      */
     public static void main(String[] args) 
     {
-        for (int i = 0; i < 6; i++)
-        {
-            int roll1 = (int)(Math.random() * 6) + 1;
-            int roll2 = (int)(Math.random() * 6) + 1;
-            int roll3 = (int)(Math.random() * 6) + 1;
-            int roll4 = (int)(Math.random() * 6) + 1;
-
-            int rolls[] = {roll1, roll2, roll3, roll4};
-            //int lowest_roll = Collections.min(Arrays.asList(rolls));
-            //int lowestRoll = 0;
-            //rollTotal -= lowestRoll;
-            int lowestRoll = 6;
-
-            for (int j = 0; j < rolls.length; j++)
-            {
-                if (lowestRoll > rolls[j])
-                {
-                    lowestRoll = rolls[j];
-                }
-            }
-
-            System.out.println(lowestRoll);
-
-            int rollTotal = roll1 + roll2 + roll3 + roll4 - lowestRoll;
-
-            System.out.println(roll1 + " + " + roll2 + " + " + roll3 + " + " + roll4 + " - " + lowestRoll + " = " + rollTotal);
-        }
+        intro();
+        rolls();
     }
 
     public static void intro()
     {
         System.out.println("Hello! Let's roll up your character's stats!");
         System.out.println("This program will roll your stats, and then you'll be able to assign them.");
+        System.out.println("Let's get started!");
     }
 
     public static void rolls()
@@ -53,13 +29,25 @@ public class rollStats
             int roll3 = (int)(Math.random() * 6) + 1;
             int roll4 = (int)(Math.random() * 6) + 1;
 
-            int rollTotal = (roll1 + roll2 + roll3 + roll4); // - //TODO subtract lowest die;
-            int[] rolls = {roll1, roll2, roll3, roll4};
-            //int lowest_roll = Collections.min(Arrays.asList(rolls));
-            int lowest_roll = 0;
-            rollTotal -= lowest_roll;
+            int rolls[] = {roll1, roll2, roll3, roll4};
+            int lowestRoll = 6;
 
-            System.out.println(rollTotal);
+            for (int j = 0; j < rolls.length; j++)
+            {
+                if (lowestRoll > rolls[j])
+                {
+                    lowestRoll = rolls[j];
+                }
+            }
+
+            int rollTotal = roll1 + roll2 + roll3 + roll4 - lowestRoll;
+
+            System.out.println(roll1 + " + " + roll2 + " + " + roll3 + " + " + roll4 + " - " + lowestRoll + " = " + rollTotal);
         }
+    }
+
+    public static void assignStats()
+    {
+        
     }
 }
